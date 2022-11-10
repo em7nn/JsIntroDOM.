cec6d7b414b0ee4ba8f07305588883fb90840676
em7nn <tu7lqt7ff@code.edu.az>
2022-11-11 00:52:43 +0300

    HomeTask

---
+++ JsIntroDOM1/ehte.js
@@ -0,0 +1,15 @@
+const btn = document.getElementById("btn").addEventListener('click', function onClick(click) {
+
+  document.body.style.backgroundColor = "blue"
+
+});
+const slw = document.getElementById("slw").addEventListener('click', function onClick(click) {
+
+    document.body.style.backgroundColor = "red"
+  
+  });
+  const wrs = document.getElementById("wrs").addEventListener('click', function onClick(click) {
+
+    document.body.style.backgroundColor = "green"
+  
+  });
